4a42a382a44769833b22f38d20279080d155e506
Jeffrey Liew <jliew626@utexas.edu>
2020-07-01 00:32:25 -0500

    Military Time --> Standard Time

--- church_database/database_html.py
+++ church_database/database_html.py
@@ -9,9 +9,11 @@ sheet = wb['Sheet1']
 
 # /Users/jeffreyliew/Documents/GitHub/Church-Database/church_database/database_html.py > /Users/jeffreyliew/Documents/GitHub/Church-Database/church_database/HTML.txt
 sys.stout = open('HTML.txt', 'w')
+# Copy paste into terminals
+# python database_html.py > HTML.txt
 
 # Setting up HTML
-setUp = "\n<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Spartan\">  <!-- Access to Google Fonts API --> \n<div style=\"overflow-x:auto;\">\n *: College Ministry Available (Check to filter) <input type=\"checkbox\" id=\"myCheck\" onclick=\"myFunction()\">\n<table id=\"myTable\">\n  <tr class=\"header\">\n    <th style=\"width:16%;\" onclick=\"sortTable(0)\">Name</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(1)\">Sunday Worship Time</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(2)\">Address</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(3)\">Distance (miles)</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(4)\">Denomination</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(5)\">Contact Info</th>\n  </tr>"
+setUp = "\n<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Spartan\">  <!-- Access to Google Fonts API --> \n<div style=\"overflow-x:auto;\">\n \U0001F392: College Ministry Available (Check to filter) <input type=\"checkbox\" id=\"myCheck\" onclick=\"myFunction()\">\n<table id=\"myTable\">\n  <tr class=\"header\">\n    <th style=\"width:16%;\" onclick=\"sortTable(0)\">Name</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(1)\">Sunday Worship Time</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(2)\">Address</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(3)\">Distance (miles)</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(4)\">Denomination</th>\n    <th style=\"width:16%;\" onclick=\"sortTable(5)\">Contact Info</th>\n  </tr>"
 print(setUp)
 
 # Initalizing the first value
@@ -23,15 +25,40 @@ emoji = ('\U0001F392')
 while(notEmpty):
     # Read the row and store in appropiate variables
     print()
-    name = sheet['A{}'.format(i)].value
-    adds = sheet['B{}'.format(i)].value
-    dist = sheet['C{}'.format(i)].value
-    deno = sheet['D{}'.format(i)].value
-    link = sheet['E{}'.format(i)].value
-    cMin = sheet['F{}'.format(i)].value
-    cont = sheet['H{}'.format(i)].value
-    time = sheet['I{}'.format(i)].value
+    name = sheet['A{}'.format(i)].value # Name
+    adds = sheet['B{}'.format(i)].value # Address
+    dist = sheet['C{}'.format(i)].value # Distance(mi)
+    deno = sheet['D{}'.format(i)].value # Denomination
+    link = sheet['E{}'.format(i)].value # Website Link
+    cMin = sheet['F{}'.format(i)].value # College Ministry
+    cont = sheet['H{}'.format(i)].value # Contact Email
+    time = sheet['I{}'.format(i)].value # Sunday Worship Time
 
+    # Formating time
+    hour = time.hour
+    min = time.minute
+    if min < 10:
+        min = '0'+str(min)
+    notEmpty = 0
+
+    # Check if AM or PM
+    apm = ''
+    if hour == 0:
+        apm = 'AM'
+    elif hour > 11:
+        apm = 'PM'
+    else:
+        apm = 'AM'
+
+    # Convert military to standard time
+    y = hour%12
+    if(y == 0):
+        hour = 12
+    time = ('{}:{} {}'.format(hour, min, apm))
+
+    print(time)
+
+'''
     # Print out HTML code
     print('    <tr>')
     # Check for college ministry
@@ -48,10 +75,13 @@ while(notEmpty):
     print('        <td>{}</td>'.format(cont))
     print('    </tr>')
 
+
+
     i = i + 1
     name = sheet['A{}'.format(i)].value
     if not name:
         notEmpty = 0
+    '''
 
 print()
 print("</table>\n</div>\n")
