0aaf2e57477784e1b5356c2088d91803413c8c7c
cce2020 <66157331+cce2020@users.noreply.github.com>
2020-08-04 04:02:09 -0500

    Update ChurchDB_JS.js
    Added JS slider functions for time and distance

--- ChurchDB_JS.js
+++ ChurchDB_JS.js
@@ -1,144 +1,317 @@
 function sortTable(n) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("myTable");
-  switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc";
-  /* Make a loop that will continue until
-  no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.rows;
-    var xgty;
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
-    for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      xgty = false;
-      // The Column for the Name:
-      if (n == 0) {
-      	x = rows[i].cells[0].innerHTML;
-      	y = rows[i + 1].cells[0].innerHTML;
-        // Get name for every church:
-        var x1 = x.substring(x.indexOf(">" + 1));
-        var y1 = y.substring(y.indexOf(">" + 1));
-        xgty = x1 > y1;
-        } 
-      // The Column for the Time:
-      else if (n == 1) {
-        // Get two elements you want to compare within the row:
-        x = rows[i].cells[1].innerHTML;
-      	y = rows[i + 1].cells[1].innerHTML;
-        // First priority is the time:
-        var x1 = x.substring(5, 6);
-        var y1 = y.substring(5, 6);
-        var strx, stry = 0;
-        // The next priority is the number:
-        if (x.indexOf(":") == 1) {
-        	strx = parseInt(x.substring(0, 1));
-        } else if (x.indexOf(":") == 2) {
-          strx = parseInt(x.substring(0, 2));
-        }
-        if (y.indexOf(":") == 1) {
-          stry = parseInt(y.substring(0, 1));
-        } else if (y.indexOf(":") == 2) {
-          stry = parseInt(y.substring(0, 2));
-        }
-        xgty = (x1 > y1) && (strx > stry);
-      }
-      //The column for everything else:
-      else {
-      	/* Get the two elements you want to compare,
-      	one from current row and one from the next: */
-      	x = rows[i].getElementsByTagName("TD")[n];
-      	y = rows[i + 1].getElementsByTagName("TD")[n];
-      	var cmpX=isNaN(parseFloat(x.innerHTML))?x.innerHTML.toLowerCase():parseFloat(x.innerHTML);
-      	var cmpY=isNaN(parseFloat(y.innerHTML))?y.innerHTML.toLowerCase():parseFloat(y.innerHTML);
-      	cmpX=(cmpX=='-')?0:cmpX;
-      	cmpY=(cmpY=='-')?0:cmpY;
-        xgty = (cmpX > cmpY);
-      }
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
-      if (dir == "asc" && xgty == true) {
-          shouldSwitch = true;
-          break;
-        }
-      else if (dir == "desc" && xgty == false) {
-         	shouldSwitch = true;
-         	break;
-        }
-      }     
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount ++;
-    } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
+	var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+	var xDate, yDate, xTimeIndex, yTimeIndex, xM, yM, xHour, yHour, xMinute, yMinute;
+	table = document.getElementById("myTable");
+	switching = true;
+	// Set the sorting direction to ascending:
+	dir = "asc";
+	/* Make a loop that will continue until
+	no switching has been done: */
+	while (switching) {
+		// Start by saying: no switching is done:
+		switching = false;
+		rows = table.rows;
+		/* Loop through all table rows (except the
+		first, which contains table headers): */
+		for (i = 1; i < (rows.length - 1); i++) {
+			// Start by saying there should be no switching:
+			shouldSwitch = false;
+			// The Column for the Name:
+			if (n == 0) {
+				x = rows[i].cells[0].innerHTML;
+				y = rows[i + 1].cells[0].innerHTML;
+				// Get name for every church:
+				var x1 = x.substring(x.indexOf(">")+1);
+				var y1 = y.substring(y.indexOf(">")+1);
+				/* Check if the two rows should switch place,
+					based on the direction, asc or desc: */
+				if (dir == "asc") {
+					if (x1 > y1) {
+						// If so, mark as a switch and break the loop:
+						shouldSwitch = true;
+						break;
+					}
+				} else if (dir == "desc") {
+					if (x1 < y1) {
+						// If so, mark as a switch and break the loop:
+						shouldSwitch = true;
+						break;
+					}
+				}
+			} // The Column for the Time:
+			else if (n == 1) {
+				x = rows[i].cells[1].innerHTML;
+				y = rows[i + 1].cells[1].innerHTML;
+
+				// Get x time
+				xDate = timeToDateObj(x)
+
+				// Get y time
+				yDate = timeToDateObj(y);
+
+				// Compare times
+				if (dir == "asc" && xDate > yDate) {
+					shouldSwitch = true;
+					break;
+				} else if (dir == "desc" && xDate < yDate) {
+					shouldSwitch = true; 
+					break;
+				}
+			} //The column for everything else:
+			else {
+				/* Get the two elements you want to compare,
+				one from current row and one from the next: */
+				x = rows[i].getElementsByTagName("TD")[n];
+				y = rows[i + 1].getElementsByTagName("TD")[n];
+				var cmpX = isNaN(parseFloat(x.innerHTML)) ? x.innerHTML.toLowerCase() : parseFloat(x.innerHTML);
+				var cmpY = isNaN(parseFloat(y.innerHTML)) ? y.innerHTML.toLowerCase() : parseFloat(y.innerHTML);
+				cmpX = (cmpX == '-') ? 0 : cmpX;
+				cmpY = (cmpY == '-') ? 0 : cmpY;
+				/* Check if the two rows should switch place,
+				based on the direction, asc or desc: */
+				if (dir == "asc") {
+					if (cmpX > cmpY) {
+						// If so, mark as a switch and break the loop:
+						shouldSwitch = true;
+						break;
+					}
+				} else if (dir == "desc") {
+					if (cmpX < cmpY) {
+						// If so, mark as a switch and break the loop:
+						shouldSwitch = true;
+						break;
+					}
+				}
+			}
+		}
+		if (shouldSwitch) {
+			/* If a switch has been marked, make the switch
+			and mark that a switch has been done: */
+			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			switching = true;
+			// Each time a switch is done, increase this count by 1:
+			switchcount++;
+		} else {
+			/* If no switching has been done AND the direction is "asc",
+			set the direction to "desc" and run the while loop again. */
+			if (switchcount == 0 && dir == "asc") {
+				dir = "desc";
+				switching = true;
+			}
+		}
+	}
 }
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
+	coll[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+		if (content.style.maxHeight) {
+			content.style.maxHeight = null;
+		} else {
+			content.style.maxHeight = content.scrollHeight + "px";
+		}
+	});
 }
 
+// Filter function once button is clicked
 function submit() {
 	// Init arrays, table, and checked checkboxes
-  var CBworshipTime, CBdistance, CBdenomination = [];
-  var CBcollegeMin = false;
-  var rows = document.getElementById("myTable").rows;
-  var checkedBoxes = document.querySelectorAll('input[type=checkbox]:checked');
-  
-  // Sort columns/categories of checked checkboxes
-  for (var cb of checkedBoxes) {
-    if (cb.name == "time") {
-    	CBworshipTime.push(cb.id);
-    } else if (cb.name == "distance") {
-    	CBdistance.push(cb.id);
-    } else if (cb.name == "denomination") {
-    	CBdenomination.push(cb.id);
-    } else if (cb.name == "collegeMin") {
-    	CBcollegeMin = true;
-    }
-  }
-  
-  // Bools to check if no checkboxes were selected for each column/category
-  var timeArrayExists = Array.isArray(CBworshipTime) && CBworshipTime.length;
-  var distanceArrayExists = Array.isArray(CBdistance) && CBdistance.length;
-  var denominationArrayExists = Array.isArray(CBdenomination) && CBdenomination.length;
-  
-  // Checks every row
-  for (var i = 1; i < rows.length; i++) {    
-    // If any of the conditions are broken, hide current row and move on to next one
-    if ((timeArrayExists && !CBworshipTime.includes(rows[i].cells[1].innerHTML)) ||
-    	  (distanceArrayExists && !CBdistance.includes(rows[i].cells[3].innerHTML)) ||
-        (denominationArrayExists && !CBdenomination.includes(rows[i].cells[4].innerHTML.toLowerCase())) ||
-        (CBcollegeMin && !rows[i].cells[0].innerHTML.includes("🎒"))) {
-    	rows[i].style.display = "none";
-      continue;
-    }
-    rows[i].style.display = "";
-  }
+	var CBcollegeMin = false;
+	var rows = document.getElementById("myTable").rows;
+	var checkedBoxes = document.querySelectorAll('input[type=checkbox]:checked');
+
+	// Sort columns/categories of checked checkboxes
+	var CBdistance = [];
+	var CBdenomination = [];
+	for (var cb of checkedBoxes) {
+		if (cb.name == "distance") {
+			CBdistance.push(cb.id);
+		} else if (cb.name == "denomination") {
+			CBdenomination.push(cb.id);
+		} else if (cb.name == "collegeMin") {
+			CBcollegeMin = true;
+		}
+	}
+
+	// Bools to check if no checkboxes were selected for each column/category
+	var distanceArrayExists = Array.isArray(CBdistance) && CBdistance.length;
+	var denominationArrayExists = Array.isArray(CBdenomination) && CBdenomination.length;
+	var inTime, inDistance, inDenomination, inCollegeMin = false;
+	var time, timeList, timeLeft, colonIndex, distance;
+	var startTime = timeToDateObj(document.getElementById("time1").innerHTML);
+	var endTime = timeToDateObj(document.getElementById("time2").innerHTML);
+
+	// Checks every row
+	// If any of the conditions are broken, hide current row and move on to next one
+	for (var i = 1; i < rows.length; i++) {
+		// Time check
+		timeList = rows[i].cells[1].innerHTML;
+		inTime = false;
+
+		if (timeList == "") {
+			timeLeft = false;
+		} else {
+			timeLeft = true;
+		}
+		if (timeList.includes("Saturday")) {
+			timeList = timeList.substring(timeList.indexOf(">")+1);
+		}
+
+		while (timeLeft && !inTime) {
+			time = timeToDateObj(timeList);
+			inTime = time >= startTime && time <= endTime;
+
+			if (timeList.length > 8) {
+				timeList = timeList.substring(timeList.indexOf(">")+1);
+			} else {
+				timeLeft = false;
+			}
+		}
+
+		// Distance Check
+		distance = Math.ceil(rows[i].cells[3].innerHTML).toString();
+		inDistance = CBdistance.includes(distance);
+
+		// Denomination and College Ministry Check 
+		inDenomination = CBdenomination.includes(rows[i].cells[4].innerHTML);
+		inCollegeMin = rows[i].cells[0].innerHTML.includes("🎒");
+
+		if ((!inTime) ||
+			(distanceArrayExists && !inDistance) ||
+			(denominationArrayExists && !inDenomination) ||
+			(CBcollegeMin && !inCollegeMin)) {
+			rows[i].style.display = "none";
+		} else {
+			rows[i].style.display = "";
+		}
+	}
+
+	// Reapply zebra pattern
+	var even = false;
+	for (i = 1; i < rows.length-1; i++) {
+		if (rows[i].style.display == "") {
+			if (even) {
+				rows[i].style.backgroundColor = "white";
+			} else {
+				rows[i].style.backgroundColor = "#f2f2f2";
+			}
+			even = !even;
+		}
+	}
 }
+
+
+// Gets time/list of times and converts first to date object
+function timeToDateObj(time) {
+	var date, colonIndex, hour, minute, xM;
+	if (time == "") {
+		date = new Date(2020, 1, 2, 0, 0);
+	} else {
+		if (time.includes("Saturday")) {
+			time = time.substring(time.indexOf(">")+1);
+		}
+		colonIndex = time.indexOf(":");
+		xM = time.substring(time.indexOf("M")-1, time.indexOf("M"));
+		hour = parseInt(time.substring(0, colonIndex));
+		minute = parseInt(time.substring(colonIndex + 1, colonIndex + 3));
+		if (xM == "P" && hour != 12) {
+			hour += 12;
+		}
+		date = new Date(2020, 1, 1, hour, minute);
+	}
+	return date;
+}
+
+// Slider function in jQuery
+// Time
+$(function() {
+  $("#slider-range").slider({
+		range: true,
+    min: 0,
+    max: 1440,
+    step: 15,
+    values: [600, 720],
+		slide: function( event, ui ) {
+			var hours1 = Math.floor(ui.values[0] / 60);
+			var minutes1 = ui.values[0] - (hours1 * 60);
+
+			if (hours1.length == 1) hours1 = '0' + hours1;
+			if (minutes1.length == 1) minutes1 = '0' + minutes1;
+			if (minutes1 == 0) minutes1 = '00';
+			if (hours1 >= 12) {
+					if (hours1 == 12) {
+							hours1 = hours1;
+							minutes1 = minutes1 + " PM";
+					} else {
+							hours1 = hours1 - 12;
+							minutes1 = minutes1 + " PM";
+					}
+			} else {
+					hours1 = hours1;
+					minutes1 = minutes1 + " AM";
+			}
+			if (hours1 == 0) {
+					hours1 = 12;
+					minutes1 = minutes1;
+			}
+
+			$('.slider-time').html(hours1 + ':' + minutes1);
+
+			var hours2 = Math.floor(ui.values[1] / 60);
+			var minutes2 = ui.values[1] - (hours2 * 60);
+
+			if (hours2.length == 1) hours2 = '0' + hours2;
+			if (minutes2.length == 1) minutes2 = '0' + minutes2;
+			if (minutes2 == 0) minutes2 = '00';
+			if (hours2 >= 12) {
+					if (hours2 == 12) {
+							hours2 = hours2;
+							minutes2 = minutes2 + " PM";
+					} else if (hours2 == 24) {
+							hours2 = 11;
+							minutes2 = "59 PM";
+					} else {
+							hours2 = hours2 - 12;
+							minutes2 = minutes2 + " PM";
+					}
+			} else {
+					hours2 = hours2;
+					minutes2 = minutes2 + " AM";
+			}
+
+			$('.slider-time2').html(hours2 + ':' + minutes2);
+		}
+	});
+});
+
+$(function() {
+	$("#slider-range2").slider({
+    range: true,
+    min: 0,
+    max: 10,
+    step: 1,
+    values: [4, 6],
+    slide: function (e, ui) {
+        var dist1 = Math.floor(ui.values[0]);
+
+				d1 = dist1;
+        dist1 = dist1 + ' mile';
+        if (d1 > 1) dist1 = dist1 + 's';
+
+
+        $('.slider-dist').html(dist1);
+
+        var dist2 = Math.floor(ui.values[1]);
+
+       	d2 = dist2;
+        dist2 = dist2 + ' mile';
+        if (d2 > 1) dist2 = dist2 + 's';
+
+        $('.slider-dist2').html(dist2);
+    	}
+	});
+});
